910eb50ce8d9617993cbd2285dc6582d77ac8556
ViviAlfaro <alfaroviviana2004 @gmail.com>
2024-09-12 14:04:35 -0600

    Sobrecarga de operadores de deferencia y direccion

--- MPointer.cpp
+++ MPointer.cpp
@@ -26,12 +26,28 @@ MPointer<T>& MPointer<T>::operator=(const MPointer& other) {
     if (this != &other) {
         MPointerGC::getInstance().removeReference(id);
         ptr = other.ptr;
+        id = other.id;
         MPointerGC::getInstance().addReference(id);
     }
     return *this;
 }
 
 template<typename T>
+MPointer<T>::~MPointer() {
     MPointerGC::getInstance().removeReference(id); //El destructor elimina la referencia al puntero en el recolector de basura mediante removeReference(id)
 }
 
+template<typename T> //Sobrecarga del operador de dereferencia (MPointer<T>::operator*)
+T& MPointer<T>::operator*() {
+    return *ptr;
+}
+
+template<typename T> //Sobrecarga del operador de dirección (MPointer<T>::operator&)
+T* MPointer<T>::operator&() {
+    return ptr;
+}
+
+template<typename T> //Obtener el valor del objeto apuntado por ptr. La palabra clave const indica que no modifica el estado interno del objeto.
+T MPointer<T>::getValue() const {
+    return *ptr;
+}
