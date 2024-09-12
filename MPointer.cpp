8bf8fbb9c63e1157bcf36482ba00f734bc9fb212
ViviAlfaro <alfaroviviana2004 @gmail.com>
2024-09-12 13:59:18 -0600

    SObrecarga del operador de asignacion

--- MPointer.cpp
+++ MPointer.cpp
@@ -13,12 +13,25 @@ MPointer<T> MPointer<T>::New() {
     return MPointer<T>();
 }
 
-template<typename T> // constructor de copia permite crear un nuevo MPointer copiando otro. Copia el puntero (ptr) y el identificador (id) del MPointer original
+template<typename T> //Constructor de copia permite crear un nuevo MPointer copiando otro. Copia el puntero (ptr) y el identificador (id) del MPointer original
 MPointer<T>::MPointer(const MPointer& other) {
     ptr = other.ptr;
     id = other.id;
     MPointerGC::getInstance().addReference(id);
 }
 
-Template<typename T>
+
+template<typename T> // Sobrecarga del operador de asignacion
 MPointer<T>& MPointer<T>::operator=(const MPointer& other) {
+    if (this != &other) {
+        MPointerGC::getInstance().removeReference(id);
+        ptr = other.ptr;
+        MPointerGC::getInstance().addReference(id);
+    }
+    return *this;
+}
+
+template<typename T>
+    MPointerGC::getInstance().removeReference(id); //El destructor elimina la referencia al puntero en el recolector de basura mediante removeReference(id)
+}
+
