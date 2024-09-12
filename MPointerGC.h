a1b06dabe5547f5f2e62e746d1cfa3a07408aaa8
ViviAlfaro <alfaroviviana2004 @gmail.com>
2024-09-12 16:38:05 -0600

    Manejo de referencias u colector de basura

--- MPointerGC.h
+++ MPointerGC.h
@@ -9,9 +9,16 @@ private:
     static MPointerGC* instance; //Puntero estático que apunta a la única instancia de la clase 
     std::unordered_map<int, int> referenceCount; //Almacena el conteo de referencias de cada puntero
     std::unordered_map<int, void*> pointers; 
-    std::unordered_map<int, void*>;
-
     MPointerGC() = default;
 
-}
+public:
+    static MPointerGC& getInstance();
 
+    int registerPointer(void* ptr);
+    void addReference(int id);
+    void removeReference(int id);
+
+    void garbageCollect(); //Método para eliminar punteros sin referencia
+};
+
+#endif
