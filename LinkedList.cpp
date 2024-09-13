56bf5c3036c9e3b6014d9e1231232183d9f69e9c
ViviAlfaro <alfaroviviana2004 @gmail.com>
2024-09-13 13:34:36 -0600

    Cabeza y cola de la lista

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -18,3 +18,45 @@ LinkedList<T>::~LinkedList() {
         current = next;
     }
 }
+
+// Añadir elementos a la lista
+template<typename T>
+void LinkedList<T>::add(T value) {
+    MPointer<Node<T>> newNode = MPointer<Node<T>>::New(); // Crear nodo con MPointer
+    newNode->data = MPointer<T>::New();
+    *newNode->data = value;
+
+    if (head == nullptr) {
+        head = newNode;
+        tail = newNode;
+    } else {
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    }
+}
+
+// Mostrar la lista
+template<typename T>
+void LinkedList<T>::display() {
+    MPointer<Node<T>> current = head;
+    while (current != nullptr) {
+        std::cout << *current->data << " ";  // Acceder al valor usando MPointer
+        current = current->next;
+    }
+    std::cout << std::endl;
+}
+
+// Obtener la cabeza de la lista
+template<typename T>
+Node<T>* LinkedList<T>::getHead() {
+    std::cout << *current->data << " ";  // Acceder al valor usando MPointer
+    current = current->next;
+    return head.get();
+}
+
+// Obtener la cola de la lista
+template<typename T>
+Node<T>* LinkedList<T>::getTail() {
+    return tail.get();
+}
