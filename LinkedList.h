a89a196bd5f103114fece0c902d51c3bb15f2b43
ViviAlfaro <alfaroviviana2004 @gmail.com>
2024-09-13 12:41:50 -0600

    Algoritmos

--- LinkedList.h
+++ LinkedList.h
@@ -7,13 +7,37 @@ template<typename T>
 class Node {
 public:
     MPointer<T> data; // MPointer para el dato
-    MPointer<Node<T>> next; // MPointer para el siguiente nodo
-    MPointer<Node<T>> prev;        // MPointer para el nodo anterior
+    MPointer<Node<T>> next;// MPointer para el siguiente nodo
+    MPointer<Node<T>> prev;// MPointer para el nodo anterior
 
     Node(T value) {
-        data = MPointer<T>::New();      // Inicializar MPointer
-        *data = value;                  // Asignar valor al dato
-        next = nullptr;                 // Inicializar punteros a null
+        data = MPointer<T>::New();// Inicializar MPointer
+        *data = value;// Asignar valor al dato
+        next = nullptr;// Inicializar punteros a null
         prev = nullptr;
     }
 };
+
+template<typename T>
+class LinkedList {
+private:
+    MPointer<Node<T>> head;// MPointer para la cabeza de la lista
+    MPointer<Node<T>> tail;// MPointer para la cola de la lista
+
+public:
+    LinkedList();
+    ~LinkedList();
+
+    void add(T value);// Añadir elementos
+    void remove(T value);// Eliminar elementos
+    void display();// Mostrar elementos
+
+    Node<T>* getHead();// Obtener el nodo head
+    Node<T>* getTail();// Obtener el nodo tail
+
+    // Algoritmos de ordenamiento
+    void quickSort();
+    void bubbleSort();
+    void insertionSort();
+
+    }
