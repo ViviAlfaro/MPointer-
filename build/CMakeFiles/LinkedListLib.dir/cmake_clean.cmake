2899c6ff54bc44a524c211b471d5af272406cf8e
ViviAlfaro <alfaroviviana2004 @gmail.com>
2024-09-13 21:56:00 -0600

    Build

---
+++ build/CMakeFiles/LinkedListLib.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinkedListLib.dir/LinkedList.cpp.o"
+  "CMakeFiles/LinkedListLib.dir/LinkedList.cpp.o.d"
+  "CMakeFiles/LinkedListLib.dir/MPointer.cpp.o"
+  "CMakeFiles/LinkedListLib.dir/MPointer.cpp.o.d"
+  "CMakeFiles/LinkedListLib.dir/MPointerGC.cpp.o"
+  "CMakeFiles/LinkedListLib.dir/MPointerGC.cpp.o.d"
+  "libLinkedListLib.a"
+  "libLinkedListLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LinkedListLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
